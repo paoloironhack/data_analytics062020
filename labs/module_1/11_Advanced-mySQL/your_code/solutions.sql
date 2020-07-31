7c945ce45dacd6f0182b57438c5bf14aa8c717e3
Frank Jan Mutsters <f.j.mutsters@gmail.com>
2020-07-31 16:58:23 +0200

    SQL advanced completed

--- labs/module_1/11_Advanced-mySQL/your_code/solutions.sql
+++ labs/module_1/11_Advanced-mySQL/your_code/solutions.sql
@@ -1,8 +1,8 @@
-#Challenge 1- Most Profiting Authors
+#Challenge 1 - Most Profiting Authors
 
 #Step 1
 
-SELECT titles.title_id as 'Title', titleauthor.au_id as 'Author', titles.advance * titleauthor.royaltyper / 100 as 'Advance', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as 'Sales Royalty'
+SELECT titles.title_id as 'Title', titleauthor.au_id as 'Author', titles.advance * titleauthor.royaltyper / 100 as 'Advance', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as 'Sales_Royalty'
 FROM titles
 INNER JOIN titleauthor
 ON titles.title_id = titleauthor.title_id
@@ -34,3 +34,43 @@ GROUP BY Author, Title) as summary2
 GROUP BY Author
 ORDER BY Profit DESC
 LIMIT 3;
+
+#Challenge 2 - Alternative Solution
+
+CREATE TEMPORARY TABLE royalty_summary
+SELECT titles.title_id as 'Title', titleauthor.au_id as 'Author', titles.advance * titleauthor.royaltyper / 100 as 'Advance', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 as 'Sales Royalty'
+FROM titles
+INNER JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+INNER JOIN sales
+ON titles.title_id = sales.title_id;
+
+#Check table
+SELECT * 
+FROM royalty_summary;
+
+CREATE TEMPORARY TABLE aggregated_royalties_summary
+SELECT Title, Author, SUM(Sales_Royalty) as 'Aggregated_royalties', SUM(Advance) as 'Aggregated_advances'
+FROM royalty_summary
+GROUP BY Author, Title;
+
+#Check temporary table
+SELECT * 
+FROM aggregated_royalties_summary;
+
+SELECT Author, SUM(Aggregated_advances + Aggregated_royalties) as 'Profit'
+FROM aggregated_royalties_summary
+GROUP BY Author
+ORDER BY Profit DESC
+LIMIT 3;
+
+#Challenge 3
+
+CREATE TABLE most_profiting_authors
+SELECT Author, SUM(Aggregated_advances + Aggregated_royalties) as 'Profit'
+FROM aggregated_royalties_summary
+GROUP BY Author;
+
+#Check table
+SELECT *
+FROM most_profiting_authors;
