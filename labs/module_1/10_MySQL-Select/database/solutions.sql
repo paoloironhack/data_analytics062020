7d51e2f37ed28e75941b1ddf1c4913216e813140
Tjerk <tjerk.kostelijk@ironhack.com>
2020-08-06 19:23:48 +0200

    nagekeken

--- labs/module_1/10_MySQL-Select/database/solutions.sql
+++ labs/module_1/10_MySQL-Select/database/solutions.sql
@@ -116,3 +116,14 @@ right join publications.authors as au
 on summary.au_id = au.au_id
 order by summary.qty desc;
 
+/*
+well done Lukas, please use capital letters for all sql commands, example:
+e.g.:
+# Challenge 4
+SELECT a.au_id AS AuthorID, a.au_lname AS LastName, a.au_fname AS FirstName, IFNULL(SUM(s.qty),0) AS Total
+FROM publications.authors a
+LEFT JOIN publications.titleauthor ta ON ta.au_id = a.au_id
+LEFT JOIN publications.sales s ON s.title_id = ta.title_id
+GROUP BY AuthorID
+ORDER BY Total DESC, AuthorID DESC;
+*/
