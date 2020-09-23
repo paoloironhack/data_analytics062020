ecd254cece453bff74b787c6e70e42a27d1eef63
Lukas Muijs <lmuijs@trading.imc.intra>
2020-09-23 20:13:57 +0200

    Made bug fixes / finalized data prep file

--- projects/Project3_Visualizations/your_code/check_distances.py
+++ projects/Project3_Visualizations/your_code/check_distances.py
@@ -3,37 +3,39 @@
 
 # In[56]:
 
-
 from pandas import json_normalize
 from opencage.geocoder import OpenCageGeocode
 from geopy import distance
 from geographiclib.geodesic import Geodesic
 import pandas as pd
 pd.options.display.float_format = '{:,.0f}'.format
+from opencage.geocoder import OpenCageGeocode
+from geopy import distance
+from geographiclib.geodesic import Geodesic
 
 
-# In[59]:
+def get_distances(location_A,lst):
+#     key = 'ddd6e78a91d647e6a33da16ae32f5c8f' 
+    key = '0e05919b28fc4b2a8761d27abf8c017e'
+    geocoder = OpenCageGeocode(key)
+    result_A = geocoder.geocode(location_A)
+    lat_A = result_A[0]['geometry']['lat']
+    lng_A = result_A[0]['geometry']['lng']
 
+    df = pd.DataFrame()
+    for B in lst:
+        dist = find_distance([lat_A,lng_A],B)
 
-# df = pd.read_csv('..\data\dtd_results_17-19.csv')
+        d = {'city' : B,
+             'distance' : dist}
 
-
-# # In[63]:
-
-
-# df['city'] = df['city'].fillna('unknown')
-
-
-# In[65]:
-
-
-
-
-# In[73]:
+        df = df.append(d,ignore_index=True)
+    return df  
 
 
 def find_distance(A,B):
     key = '0e05919b28fc4b2a8761d27abf8c017e'  # get api key from:  https://opencagedata.com
+    # key = 'ddd6e78a91d647e6a33da16ae32f5c8f'
     from opencage.geocoder import OpenCageGeocode
     from geopy import distance
     from geographiclib.geodesic import Geodesic
@@ -41,41 +43,15 @@ def find_distance(A,B):
     geocoder = OpenCageGeocode(key)
         
     Geo = Geodesic.WGS84 
-    
-    
-    result_A = geocoder.geocode(A)
-    lat_A = result_A[0]['geometry']['lat']
-    lng_A = result_A[0]['geometry']['lng']
-    
+       
     try: 
         result_B = geocoder.geocode(B)
         lat_B = result_B[0]['geometry']['lat']
         lng_B = result_B[0]['geometry']['lng']  
 
-        d = Geo.Inverse(lat_A, lng_A,  lat_B, lng_B)
+        d = Geo.Inverse(A[0], A[1],  lat_B, lng_B)
         return d['s12']
     except IndexError:
         return None
-        
-                                    
-
-
-# In[75]:
-
-
-def get_distances(location_A,lst):
-
-    df = pd.DataFrame()
-    for B in lst:
-        dist = find_distance(location_A,B)
-
-        d = {'City' : B,
-             'distance' : dist}
-
-        df = df.append(d,ignore_index=True)
-    return df    
-
-
-# In[ ]:
 
 
